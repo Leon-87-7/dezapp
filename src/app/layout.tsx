b610664e4b9041da9c1ae4dd93c1c595c8a5535d
LeonEidelman <leoneidelman09@gmail.com>
2026-01-05 23:30:48 +0200

    layput fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,13 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
+} from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/shared/app-sidebar';
+import { Separator } from '@/components/ui/separator';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
 import './globals.css';
@@ -38,21 +45,31 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          {children}
-          <Toaster />
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-14 items-center  gap-4 border-b bg-background px-4 lg:px-6">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="h-6"
+                />
+                <SignedOut>
+                  <SignInButton />
+                  <SignUpButton>
+                    <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                      Sign Up
+                    </button>
+                  </SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                  <UserButton /> //TODO move the avatar to the right
+                </SignedIn>
+              </header>
+              <main className="flex-1 overflow-auto">{children}</main>
+              <Toaster />
+            </SidebarInset>
+          </SidebarProvider>
         </body>
       </html>
     </ClerkProvider>
